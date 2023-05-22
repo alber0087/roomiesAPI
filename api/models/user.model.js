db0ffdb1fe0679a850630124e8fd00a6c303946a
Alber <albertorg0087@gmail.com>
2023-05-22 14:40:53 +0100

    Create files and folders, db connection and user model

---
+++ api/models/user.model.js
@@ -0,0 +1,45 @@
+const sequelize = require('../../db')
+const { DataTypes } = require('sequelize')
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: { 
+    type: DataTypes.STRING, 
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  gender: {
+    type: DataTypes.ENUM(['male', 'female']),
+    allowNull: false
+  },
+  smoker: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+  },
+  schedule: {
+    type: DataTypes.ENUM(['morning', 'afternoon', 'night']),
+    allowNull: true,
+  },
+  birth_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM(['user', 'admin']),
+    allowNull: false,
+    defaultValue: 'user'
+  }
+})
+
+module.exports = User
---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db/index')
+
+
+
+
+const api = express()
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    console.log('Connection has been established successfully.')
+  } catch (err) {
+    throw new Error('Cannot connect to the database')
+  }
+}
+
+const start = async () => {
+  try {
+    api.listen(process.env.PORT || 5000)
+    await connectDB()
+    console.info(`Server running on port ${process.env.PORT}`)
+  } catch (err) {
+    throw new Error(`Cannot start server on port ${process.env.PORT}`)
+  }
+}
+
+start()
+
