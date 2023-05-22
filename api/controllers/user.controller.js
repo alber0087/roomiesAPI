7d53140f4f526564190a3d5654803783819bb570
Alber <albertorg0087@gmail.com>
2023-05-22 17:18:41 +0100

    Users CRUD done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,4 +9,55 @@ async function createUser(req, res) {
   }
 }
 
-module.exports = { createUser }
+async function getAllUsers(req, res) {
+  try {
+    const user = await User.findAll()
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
+
+async function getOneUser(req, res) {
+  try {
+    const user = await User.findByPk(req.params.id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
+
+async function updateUser(req, res) {
+  try {
+    const [userExist, user] = await User.update(req.body, {
+      returning: true,
+      where: { id: req.params.id },
+    })
+
+    if (userExist !== 0) {
+      return res.status(200).json({ message: 'User updated successfully', user: user })
+    } else {
+      return res.status(404).send('User not found')
+    }
+
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    const user = await User.destroy({
+      where: { id: req.params.id }
+    })
+    if (user) {
+      return res.status(200).send('User removed successfully')
+    } else {
+      return res.status(404).send('User not found')
+    }
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+}
+
+module.exports = { createUser, getAllUsers, updateUser, getOneUser, deleteUser }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
-const { createUser } = require('../controllers/user.controller')
+const { createUser, getAllUsers, updateUser, getOneUser, deleteUser } = require('../controllers/user.controller')
 
 router.post('/', createUser)
+router.get('/', getAllUsers)
+router.put('/:id', updateUser)
+router.get('/:id', getOneUser)
+router.delete('/:id', deleteUser)
 
 module.exports = router
