4a326a35b5c8bf409cf96eef8b5e2d4a36d6ced9
Alber <albertorg0087@gmail.com>
2023-05-24 14:25:58 +0100

    Fixing problems

--- SQL/communities.sql
+++ SQL/communities.sql
@@ -1,3 +1,3 @@
-INSERT INTO communities (name, address, rooms) VALUES ('Parkview Apartments', '123 Main Street', 2);
-INSERT INTO communities (name, address, rooms) VALUES ('Maple Grove Condos', '456 Elm Avenue', 1);
+INSERT INTO communities (name, address, rooms) VALUES ('Parkview Apartments', '123 Main Street', 4);
+INSERT INTO communities (name, address, rooms) VALUES ('Maple Grove Condos', '456 Elm Avenue', 5);
 INSERT INTO communities (name, address, rooms) VALUES ('Sunset Villas', '789 Oak Lane', 3);
