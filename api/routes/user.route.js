cee3e6e2ca59c7f16dd1334f935cf96f9b8154af
ayo <ayopro89@gmail.com>
2023-05-24 14:53:38 +0100

    USER Added  selfUpdate function and selfDelete

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const { createUser, getAllUsers, updateUser, getOneUser, deleteUser } = require('../controllers/user.controller')
+const { createUser, getAllUsers, updateUser, getOneUser, deleteUser, selfDelete, selfUpdate } = require('../controllers/user.controller')
 const { checkAdmin } = require('../middlewares/auth')
 
 router.post('/', checkAdmin, createUser)
@@ -8,5 +8,8 @@ router.get('/', checkAdmin, getAllUsers)
 router.put('/:id', checkAdmin, updateUser)
 router.get('/:id', checkAdmin, getOneUser)
 router.delete('/:id', checkAdmin, deleteUser)
+
+router.delete('/delete', selfDelete)
+router.put('/update/:id', selfUpdate)
 
 module.exports = router
