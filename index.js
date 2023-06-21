d28e444d5d3f4daead713b5fa59adcd55a2b0262
adrGlez <127108683+adrGlez@users.noreply.github.com>
2023-06-21 14:21:51 +0100

    Socket.io added

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ const connectDB = async () => {
   }
 }
 
+const http = require('http')
+const server = http.createServer()
+
+const io = require('socket.io')(server, {
+  cors: { origin: '*' }
+})
+
+io.on('connection', (socket) => {
+  console.log('Client connected')
+
+  socket.broadcast.emit('chat_message', {
+    user: 'INFO',
+    message: 'New roomie connected'
+  })
+
+  socket.on('chat_message', (data) => {
+    io.emit('chat_message', data)
+  })
+})
+
+server.listen(3001)
+
 const start = async () => {
   try {
     api.use(cors())
